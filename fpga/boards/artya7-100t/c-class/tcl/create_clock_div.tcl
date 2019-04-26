81b21539103ad8deb981e46b4535dc5ccb268b87
Neel <neelgala@gmail.com>
2019-04-26 18:54:28 +0530

    adding clock divider tcl for vivado

---
+++ fpga/boards/artya7-100t/c-class/tcl/create_clock_div.tcl
@@ -0,0 +1,37 @@
+set curdir [ file dirname [ file normalize [ info script ] ] ]
+source $curdir/env.tcl
+
+
+open_project $ip_project_dir/$ip_project.xpr
+set_property "simulator_language" "Mixed" [current_project]
+set_property "target_language" "Verilog" [current_project]
+
+if { [get_ips -quiet clk_divider] eq "" } {
+    create_ip -name clk_wiz -vendor xilinx.com -library ip -version 6.0 -module_name clk_divider 
+} else {
+    reset_run clk_divider_synth_1
+}
+
+set_property -dict [list \
+                        CONFIG.PRIMITIVE {MMCM} \
+                        CONFIG.RESET_TYPE {ACTIVE_LOW} \
+                        CONFIG.RESET_PORT {resetn} \
+                        CONFIG.MMCM_DIVCLK_DIVIDE {1} \
+                        CONFIG.MMCM_CLKFBOUT_MULT_F {10} \
+                        CONFIG.MMCM_COMPENSATION {ZHOLD} \
+                        CONFIG.MMCM_CLKOUT0_DIVIDE_F {48} \
+                        CONFIG.PRIM_IN_FREQ {100} \
+                        CONFIG.CLK_IN1_BOARD_INTERFACE {sys_clock} \
+                        CONFIG.CLKOUT1_REQUESTED_OUT_FREQ {20.833} \
+                        CONFIG.CLKOUT1_DRIVES {BUFG} \
+                        CONFIG.CLKOUT1_USED {1} \
+                        CONFIG.CLKOUT2_REQUESTED_OUT_FREQ {166.667} \
+                        CONFIG.CLKOUT2_DRIVES {BUFG} \
+                        CONFIG.CLKOUT2_USED {1} \
+                        CONFIG.CLKOUT3_REQUESTED_OUT_FREQ {200.000} \
+                        CONFIG.CLKOUT3_DRIVES {BUFG} \
+                        CONFIG.CLKOUT3_USED {1} ] [get_ips clk_divider]
+
+create_ip_run [get_ips clk_divider]
+launch_run clk_divider_synth_1
+wait_on_run clk_divider_synth_1
