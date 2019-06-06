b4d03adf22ea082c8faa02a5fe8e2f1700b0a222
Neel <neelgala@gmail.com>
2019-06-06 17:27:06 +0530

    removed DDR interface

--- fpga/boards/artya7-35t/e-class/fpga_top.v
+++ fpga/boards/artya7-35t/e-class/fpga_top.v
@@ -33,30 +33,6 @@ module fpga_top#
    parameter AXI_ADDR_WIDTH = 28
    )
   (
-   // ---- DDR ports --------------//
-   // Inouts
-  /* inout [15:0]   ddr3_dq,
-   inout [1:0]    ddr3_dqs_n,
-   inout [1:0]    ddr3_dqs_p,
-
-   output [13:0] ddr3_addr,
-   output [2:0]  ddr3_ba,
-   output        ddr3_ras_n,
-   output        ddr3_cas_n,
-   output        ddr3_we_n,
-   output        ddr3_reset_n,
-   output        ddr3_ck_p,
-   output        ddr3_ck_n,
-   output        ddr3_cke,
-   
-   output        ddr3_cs_n,
-   
-   output [1:0]  ddr3_dm,
-  
-   output        ddr3_odt,
-    
-   output        pin_aresetn,
-   output        init_calib_complete,*/
    
    // ---- JTAG ports ------- //
    input         pin_tck,
@@ -66,24 +42,24 @@ module fpga_top#
    output        pin_tdo,
    
    // ---- UART ports --------//
-   input         uart_SIN,
-   output        uart_SOUT,   
+   input         uart0_SIN,
+   output        uart0_SOUT,   
 
    // ---- I2C ports --------//
    inout         i2c_sda,
    inout         i2c_scl,
    
    // ---- SPI ports ---------//
-   output       spi_mosi,
-   output       spi_sclk,
-   output       spi_nss,
-   input        spi_miso,
+   output       spi0_mosi,
+   output       spi0_sclk,
+   output       spi0_nss,
+   input        spi0_miso,
 
    // ---- GPIO ports --------//
    inout[15:0] gpio,
 
    // ---- External interrupts ports --------//
-   input [7:0] interrupts,
+   input [1:0] interrupts,
 
    // ---- System Reset ------//
    input         sys_rst,	//Active Low
@@ -104,90 +80,6 @@ module fpga_top#
    wire                            mmcm_locked;    // indicates the ui clock from mig is stable
    reg                             aresetn;
    reg                             aresetn_s;
-  
-   // Signals driven by axi converter to DDR slave ports
-   wire [AXI_ID_WIDTH-1:0]           m_axi_awid;
-   wire [AXI_ADDR_WIDTH-1:0]         m_axi_awaddr;
-   wire [7:0]                        m_axi_awlen;
-   wire [2:0]                        m_axi_awsize;
-   wire [1:0]                        m_axi_awburst;
-   wire [0:0]                        m_axi_awlock;
-   wire [3:0]                        m_axi_awcache;
-   wire [2:0]                        m_axi_awprot;
-   wire                              m_axi_awvalid;
-   wire                              m_axi_awready;
-
-   wire [63:0]                      m_axi_wdata;
-   wire [7:0]                       m_axi_wstrb;
-   wire                              m_axi_wlast;
-   wire                              m_axi_wvalid;
-   wire                              m_axi_wready;
-
-   wire                              m_axi_bready;
-   wire [AXI_ID_WIDTH-1:0]           m_axi_bid;
-   wire [1:0]                        m_axi_bresp;
-   wire                              m_axi_bvalid;
-
-   wire [AXI_ID_WIDTH-1:0]           m_axi_arid;
-   wire [AXI_ADDR_WIDTH-1:0]         m_axi_araddr;
-   wire [7:0]                        m_axi_arlen;
-   wire [2:0]                        m_axi_arsize;
-   wire [1:0]                        m_axi_arburst;
-   wire [0:0]                        m_axi_arlock;
-   wire [3:0]                        m_axi_arcache;
-   wire [2:0]                        m_axi_arprot;
-   wire                              m_axi_arvalid;
-   wire                              m_axi_arready;
-
-   wire                              m_axi_rready;
-   wire [AXI_ID_WIDTH-1:0]           m_axi_rid;
-   wire [127:0]                      m_axi_rdata;
-   wire [1:0]                        m_axi_rresp;
-   wire                              m_axi_rlast;
-   wire                              m_axi_rvalid;
-
-   // Signals from SoC Master to axi converter
-   wire [AXI_ID_WIDTH-1:0]           s_axi_awid;
-   wire [AXI_ADDR_WIDTH-1:0]         s_axi_awaddr;
-   wire [7:0]                        s_axi_awlen;
-   wire [2:0]                        s_axi_awsize;
-   wire [1:0]                        s_axi_awburst;
-   wire [0:0]                        s_axi_awlock;
-   wire [3:0]                        s_axi_awcache;
-   wire [2:0]                        s_axi_awprot;
-   wire                              s_axi_awvalid;
-   wire                              s_axi_awready;
-
-   wire [63:0]                       s_axi_wdata;
-   wire [7:0]                        s_axi_wstrb;
-   wire                              s_axi_wlast;
-   wire                              s_axi_wvalid;
-   wire                              s_axi_wready;
-   
-   wire                              s_axi_bready;
-   wire [AXI_ID_WIDTH-1:0]           s_axi_bid;
-   wire [1:0]                        s_axi_bresp;
-   wire                              s_axi_bvalid;
-   
-   wire [AXI_ID_WIDTH-1:0]           s_axi_arid;
-   wire [AXI_ADDR_WIDTH-1:0]         s_axi_araddr;
-   wire [7:0]                        s_axi_arlen;
-   wire [2:0]                        s_axi_arsize;
-   wire [1:0]                        s_axi_arburst;
-   wire [0:0]                        s_axi_arlock;
-   wire [3:0]                        s_axi_arcache;
-   wire [2:0]                        s_axi_arprot;
-   wire                              s_axi_arvalid;
-   wire                              s_axi_arready;
-   
-   wire                              s_axi_rready;
-   wire [AXI_ID_WIDTH-1:0]           s_axi_rid;
-   wire [63:0]                       s_axi_rdata;
-   wire [1:0]                        s_axi_rresp;
-   wire                              s_axi_rlast;
-   wire                              s_axi_rvalid;
-
-   wire [11:0]                       device_temp;
 
    wire i2c_scl_out, i2c_scl_in, i2c_scl_out_en;
    wire i2c_sda_out, i2c_sda_in, i2c_sda_out_en;
@@ -196,12 +88,6 @@ module fpga_top#
    
    // ---------------------------------------------------------------------------- //
    
-   // --------- Address width truncation and Reset generation for SoC ------------ //
-   wire [31:0] temp_s_axi_awaddr, temp_s_axi_araddr;
-   assign s_axi_awaddr= temp_s_axi_awaddr [AXI_ADDR_WIDTH-1:0];
-   assign s_axi_araddr= temp_s_axi_araddr [AXI_ADDR_WIDTH-1:0];
-   assign soc_reset = locked ;
-   // ---------------------------------------------------------------------------- //
 
    // ---------- Clock divider ----------------//
    
@@ -214,209 +100,106 @@ module fpga_top#
                        .locked(locked) );
    // ----------------------------------------- //
 
-   // ------------ MIG for DDR3 ---------------//
-
-   /*mig_ddr3 mig_ddr3 (
-        // Memory interface ports
-       .ddr3_addr                      (ddr3_addr),
-       .ddr3_ba                        (ddr3_ba),
-       .ddr3_cas_n                     (ddr3_cas_n),
-       .ddr3_ck_n                      (ddr3_ck_n),
-       .ddr3_ck_p                      (ddr3_ck_p),
-       .ddr3_cke                       (ddr3_cke),
-       .ddr3_ras_n                     (ddr3_ras_n),
-       .ddr3_we_n                      (ddr3_we_n),
-       .ddr3_dq                        (ddr3_dq),
-       .ddr3_dqs_n                     (ddr3_dqs_n),
-       .ddr3_dqs_p                     (ddr3_dqs_p),
-       .ddr3_reset_n                   (ddr3_reset_n),
-       .init_calib_complete            (init_calib_complete),
-     
-       .ddr3_cs_n                      (ddr3_cs_n),
-       .ddr3_dm                        (ddr3_dm),
-       .ddr3_odt                       (ddr3_odt),
-       .ui_clk                         (clk),
-       .ui_clk_sync_rst                (rst),
-
-       .mmcm_locked                    (mmcm_locked),
-       .aresetn                        (aresetn),
-       .app_sr_req                     ( 1'b0),
-       .app_ref_req                    ( 1'b0),
-       .app_zq_req                     ( 1'b0),
-       .app_sr_active                  (),
-       .app_ref_ack                    (),
-       .app_zq_ack                     (),
-
-       // Slave Interface Write Address Ports
-       .s_axi_awid                     (m_axi_awid),
-       .s_axi_awaddr                   (m_axi_awaddr),
-       .s_axi_awlen                    (m_axi_awlen),
-       .s_axi_awsize                   (m_axi_awsize),
-       .s_axi_awburst                  (m_axi_awburst),
-       .s_axi_awlock                   (m_axi_awlock),
-       .s_axi_awcache                  (m_axi_awcache),
-       .s_axi_awprot                   (m_axi_awprot),
-       .s_axi_awqos                    (4'h0),
-       .s_axi_awvalid                  (m_axi_awvalid),
-       .s_axi_awready                  (m_axi_awready),
-       // Slave Interface Write Data Ports
-       .s_axi_wdata                    (m_axi_wdata),
-       .s_axi_wstrb                    (m_axi_wstrb),
-       .s_axi_wlast                    (m_axi_wlast),
-       .s_axi_wvalid                   (m_axi_wvalid),
-       .s_axi_wready                   (m_axi_wready),
-       // Slave Interface Write Response Ports
-       .s_axi_bid                      (m_axi_bid),
-       .s_axi_bresp                    (m_axi_bresp),
-       .s_axi_bvalid                   (m_axi_bvalid),
-       .s_axi_bready                   (m_axi_bready),
-       // Slave Interface Read Address Ports
-       .s_axi_arid                     (m_axi_arid),
-       .s_axi_araddr                   (m_axi_araddr),
-       .s_axi_arlen                    (m_axi_arlen),
-       .s_axi_arsize                   (m_axi_arsize),
-       .s_axi_arburst                  (m_axi_arburst),
-       .s_axi_arlock                   (m_axi_arlock),
-       .s_axi_arcache                  (m_axi_arcache),
-       .s_axi_arprot                   (m_axi_arprot),
-       .s_axi_arqos                    (4'h0),
-       .s_axi_arvalid                  (m_axi_arvalid),
-       .s_axi_arready                  (m_axi_arready),
-       // Slave Interface Read Data Ports
-       .s_axi_rid                      (m_axi_rid),
-       .s_axi_rdata                    (m_axi_rdata),
-       .s_axi_rresp                    (m_axi_rresp),
-       .s_axi_rlast                    (m_axi_rlast),
-       .s_axi_rvalid                   (m_axi_rvalid),
-       .s_axi_rready                   (m_axi_rready),
-
-       .sys_clk_i                      (ddr3_main),
-       .clk_ref_i                      (ddr3_ref),
-       .device_temp                    (device_temp),
-       .sys_rst                        (locked)
-   );*/
 
    always @(posedge clk) begin
      aresetn <= ~rst;
    end
    
-   //assign pin_aresetn= rst;
-
-   // Instantiating the clock converter between the SoC and DDR3 MIG
-   /*clk_converter clock_converter (
-       .s_axi_aclk(core_clk),
-       .s_axi_aresetn(locked),
-       .s_axi_awid(s_axi_awid),
-       .s_axi_awaddr(s_axi_awaddr),
-       .s_axi_awlen(s_axi_awlen),
-       .s_axi_awsize(s_axi_awsize),
-       .s_axi_awburst(s_axi_awburst),
-       .s_axi_awlock(1'b0),
-       .s_axi_awcache(4'b10),
-       .s_axi_awprot(s_axi_awprot),
-       .s_axi_awregion(4'b0),
-       .s_axi_awqos(4'b0),
-       .s_axi_awvalid(s_axi_awvalid),
-       .s_axi_awready(s_axi_awready),
-       .s_axi_wdata(s_axi_wdata),
-       .s_axi_wstrb(s_axi_wstrb),
-       .s_axi_wlast(s_axi_wlast),
-       .s_axi_wvalid(s_axi_wvalid),
-       .s_axi_wready(s_axi_wready),
-       .s_axi_bid(s_axi_bid),
-       .s_axi_bresp(s_axi_bresp),
-       .s_axi_bvalid(s_axi_bvalid),
-       .s_axi_bready(s_axi_bready),
-       .s_axi_arid(s_axi_arid),
-       .s_axi_araddr(s_axi_araddr),
-       .s_axi_arlen(s_axi_arlen),
-       .s_axi_arsize(s_axi_arsize),
-       .s_axi_arburst(s_axi_arburst),
-       .s_axi_arlock(1'b0),
-       .s_axi_arcache(4'b10),
-       .s_axi_arprot(s_axi_arprot),
-       .s_axi_arregion(4'b0),
-       .s_axi_arqos(4'b0),
-       .s_axi_arvalid(s_axi_arvalid),
-       .s_axi_arready(s_axi_arready),
-       .s_axi_rid(s_axi_rid),
-       .s_axi_rdata(s_axi_rdata),
-       .s_axi_rresp(s_axi_rresp),
-       .s_axi_rlast(s_axi_rlast),
-       .s_axi_rvalid(s_axi_rvalid),
-       .s_axi_rready(s_axi_rready),
-       .m_axi_aclk(clk),
-       .m_axi_aresetn(aresetn),
-       .m_axi_awid(m_axi_awid),
-       .m_axi_awaddr(m_axi_awaddr),
-       .m_axi_awlen(m_axi_awlen),
-       .m_axi_awsize(m_axi_awsize),
-       .m_axi_awburst(m_axi_awburst),
-       .m_axi_awlock(m_axi_awlock),
-       .m_axi_awcache(m_axi_awcache),
-       .m_axi_awprot(m_axi_awprot),
-       .m_axi_awregion(),
-       .m_axi_awqos(),
-       .m_axi_awvalid(m_axi_awvalid),
-       .m_axi_awready(m_axi_awready),
-       .m_axi_wdata(m_axi_wdata),
-       .m_axi_wstrb(m_axi_wstrb),
-       .m_axi_wlast(m_axi_wlast),
-       .m_axi_wvalid(m_axi_wvalid),
-       .m_axi_wready(m_axi_wready),
-       .m_axi_bid(m_axi_bid),
-       .m_axi_bresp(m_axi_bresp),
-       .m_axi_bvalid(m_axi_bvalid),
-       .m_axi_bready(m_axi_bready),
-       .m_axi_arid(m_axi_arid),
-       .m_axi_araddr(m_axi_araddr),
-       .m_axi_arlen(m_axi_arlen),
-       .m_axi_arsize(m_axi_arsize),
-       .m_axi_arburst(m_axi_arburst),
-       .m_axi_arlock(m_axi_arlock),
-       .m_axi_arcache(m_axi_arcache),
-       .m_axi_arprot(m_axi_arprot),
-       .m_axi_arregion(),
-       .m_axi_arqos(),
-       .m_axi_arvalid(m_axi_arvalid),
-       .m_axi_arready(m_axi_arready),
-       .m_axi_rid(m_axi_rid),
-       .m_axi_rdata(m_axi_rdata),
-       .m_axi_rresp(m_axi_rresp),
-       .m_axi_rlast(m_axi_rlast),
-       .m_axi_rvalid(m_axi_rvalid), 
-       .m_axi_rready(m_axi_rready)
-   );*/
-
    
    // ---- Instantiating the C-class SoC -------------//
    mkSoc core(
        // Main Clock and Reset to the SoC
-       .CLK(core_clk),
-       .RST_N(soc_reset),
+        .CLK(core_clk),
+        .RST_N(soc_reset),
        
        // JTAG port definitions
-       .CLK_tck_clk(pin_tck),
-       .RST_N_trst(pin_trst),
-       .wire_tms_tms_in(pin_tms),
-       .wire_tdi_tdi_in(pin_tdi),
-       .wire_tdo(pin_tdo),
+        .CLK_tck_clk(pin_tck),
+        .RST_N_trst(pin_trst),
+        .wire_tms_tms_in(pin_tms),
+        .wire_tdi_tdi_in(pin_tdi),
+        .wire_tdo(pin_tdo),
+
+			 //GPIO ports
+        .gpio_io_gpio_in_inp(gpio_in),
+        .gpio_io_gpio_out(gpio_out),
+        .gpio_io_gpio_out_en(gpio_out_en),
+
+      // PWM ports
+        .pwm0_io_pwm_o(),
+        .pwm1_io_pwm_o(),
+        .pwm2_io_pwm_o(),
+        .pwm3_io_pwm_o(),
+        .pwm4_io_pwm_o(),
+        .pwm5_io_pwm_o(),
+    
+      // SPI ports
+        .spi0_io_mosi(spi0_mosi),
+        .spi0_io_sclk(spi0_sclk),
+        .spi0_io_nss(spi0_nss),
+	      .spi0_io_miso_dat(spi0_miso),
+        
+        .spi1_io_mosi(),
+        .spi1_io_sclk(),
+        .spi1_io_nss(),
+	      .spi1_io_miso_dat(),
+
+        .spi2_io_mosi(),
+        .spi2_io_sclk(),
+        .spi2_io_nss(),
+	      .spi2_io_miso_dat(),
 
        // UART port definitions
-       .uart0_io_SIN(uart_SIN),
-       .uart0_io_SOUT(uart_SOUT),
+        .uart0_io_SIN(uart0_SIN),
+        .uart0_io_SOUT(uart0_SOUT),
        
-       // SPI port definitions
-       .spi0_io_mosi(spi_mosi),
-       .spi0_io_sclk(spi_sclk),
-	   .spi0_io_nss(spi_nss),
-	   .spi0_io_miso_dat(spi_miso),
+        .uart1_io_SIN(),
+        .uart1_io_SOUT(),
+       
+        .uart2_io_SIN(),
+        .uart2_io_SOUT(),
+        
+			 //I2C ports
+		    .i2c_out_scl_out (i2c_scl_out),
+        .i2c_out_scl_in_in(i2c_scl_in),
+        .i2c_out_scl_out_en(i2c_scl_out_en),
+        .i2c_out_sda_out(i2c_sda_out),
+        .i2c_out_sda_in_in(i2c_sda_in),
+        .i2c_out_sda_out_en(i2c_sda_out_en),
 
-       
-			 //GPIO ports
-       .gpio_io_gpio_in_inp(gpio_in),
-       .gpio_io_gpio_out(gpio_out),
-       .gpio_io_gpio_out_en(gpio_out_en)
+      // XADC connection
+	      .xadc_master_awvalid(),
+	      .xadc_master_awaddr(),
+//	      .xadc_master_awprot(),
+//	      .xadc_master_awsize(),
+	      .xadc_master_m_awready_awready(),
+	      .xadc_master_wvalid(),
+	      .xadc_master_wdata(),
+	      .xadc_master_wstrb(),
+	      .xadc_master_m_wready_wready(),
+	      .xadc_master_m_bvalid_bvalid(),
+	      .xadc_master_m_bvalid_bresp(),
+	      .xadc_master_bready(),
+	      .xadc_master_arvalid(),
+	      .xadc_master_araddr(),
+//	      .xadc_master_arprot(),
+//	      .xadc_master_arsize(),
+	      .xadc_master_m_arready_arready(),
+	      .xadc_master_m_rvalid_rvalid(),
+	      .xadc_master_m_rvalid_rresp(),
+	      .xadc_master_m_rvalid_rdata(),
+	      .xadc_master_rready(),
+
+	      .qspi_io_clk_o(),
+	      .qspi_io_io_o(),
+	      .qspi_io_io0_sdio_ctrl(),
+	      .qspi_io_io1_sdio_ctrl(),
+	      .qspi_io_io2_sdio_ctrl(),
+	      .qspi_io_io3_sdio_ctrl(),
+	      .qspi_io_io_enable(),
+	      .qspi_io_io_i_io_i(),
+	      .qspi_io_ncs_o(),
+
+        .ext_interrupts_i(interrupts)
    );
 
    // ---- Instantiating the C-class SoC -------------//
