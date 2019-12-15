b5b982f323b26e2d48e0f52fe132aa609ab4ad8f
Sadhana S <edm15b039@iiitdm.ac.in>
2019-12-15 17:06:17 +0530

    Driver code for UART with pinmux configuration

--- asic/e-class-aardonyx/benchmarks/hello-shakti/hello.c
+++ asic/e-class-aardonyx/benchmarks/hello-shakti/hello.c
@@ -1,5 +1,14 @@
 #include "util.h"
+#define PINMUX_CFG   0x00040310
+int* pinmux_cfg = (int*) PINMUX_CFG;
+
+void set_spi(int* addr, int val)
+{
+    *addr = val;
+}
+
 int main(){
+	set_spi(pinmux_cfg,0x55);
   printf("Hello World from SHAKTI");
   return 0;
 }
